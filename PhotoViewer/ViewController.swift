bdc2e530e29e6acc40f816217d4c6234cae498fb
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2017-05-02 12:42:39 +0700

    Initial Commit

---
+++ PhotoViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoViewer
+//
+//  Created by Kittitat Rodphotong on 5/2/2560 BE.
+//  Copyright © 2560 Kittitat Rodphotong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
